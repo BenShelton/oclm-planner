e64e36e119b0a8f9103bf88795ac9d620c33ccc8
BenShelton <bensheltonjones@gmail.com>
2018-10-28 22:49:30 +0000

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,19 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
+  extends: [
+    'plugin:vue/recommended',
     '@vue/standard'
   ],
   rules: {
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: {
+        max: 1,
+        allowFirstLine: false
+      }
+    }],
+    'vue/no-spaces-around-equal-signs-in-attribute': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
