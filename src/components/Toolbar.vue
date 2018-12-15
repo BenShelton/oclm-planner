ceaf6d10762c3cb606b584f94247a18278f26d66
BenShelton <bensheltonjones@gmail.com>
2018-12-15 16:42:07 +0000

    add logout/login to toolbar based on state

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,18 +1,40 @@
 <template>
   <VToolbar app>
-    <VToolbarSideIcon @click.stop="toggleDrawer" />
-    <VToolbarTitle>{{ title }}</VToolbarTitle>
+    <VToolbarSideIcon v-if="loggedIn" @click.stop="toggleDrawer" />
+    <VToolbarTitle>
+      OCLM Planner | {{ title }}
+    </VToolbarTitle>
+    <VSpacer />
+    <VToolbarItems>
+      <VBtn
+        flat
+        :disabled="loading"
+        :loading="loading"
+        @click="onButtonClick"
+      >
+        {{ loggedIn ? 'Logout' : 'Login' }}
+      </VBtn>
+    </VToolbarItems>
   </VToolbar>
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
+import { mapGetters, mapActions, mapMutations } from 'vuex'
 import routes from '@/router/routes'
 
 export default {
   name: 'Toolbar',
 
+  data () {
+    return {
+      loading: false
+    }
+  },
+
   computed: {
+    ...mapGetters({
+      loggedIn: 'auth/hasToken'
+    }),
     title () {
       switch (this.$route.name) {
         case routes.HOME:
@@ -26,15 +48,38 @@ export default {
         case routes.HELP:
           return 'Help'
         default:
-          return 'OCLM Planner'
+          return 'Untitled'
       }
     }
   },
 
   methods: {
+    ...mapActions({
+      logout: 'auth/logout'
+    }),
     ...mapMutations({
-      toggleDrawer: 'drawer/TOGGLE_OPEN'
-    })
+      toggleDrawer: 'drawer/TOGGLE_OPEN',
+      alert: 'alert/UPDATE_ALERT'
+    }),
+    onButtonClick () {
+      if (!this.loggedIn) {
+        this.$router.push({ name: routes.LOGIN })
+        return
+      }
+      this.loading = true
+      this.logout()
+        .then(() => {
+          this.alert({ text: 'Successfully logged out', color: 'success' })
+          this.$router.push({ name: routes.HOME })
+        })
+        .catch(err => {
+          this.alert({ text: 'Logout failed', color: 'error' })
+          console.error(err)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    }
   }
 }
 </script>
