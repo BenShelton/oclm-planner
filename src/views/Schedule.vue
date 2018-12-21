022a30b3fbab1904f1560f64b7fbd5ca49e51bb5
BenShelton <bensheltonjones@gmail.com>
2018-12-21 10:50:57 +0000

    add schedule week navigation

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,15 +1,32 @@
 <template>
-  <VLayout fill-height justify-center class="px-2">
-    <VFlex
-      v-for="week in visibleWeeks"
-      :key="week.weekDate"
-      class="ma-2 xs12 sm6 md4 lg3 xl2 grow"
-    >
-      <ScheduleWeek
-        :week-date="week.weekDate"
-        :current="week.current"
-      />
-    </VFlex>
+  <VLayout fill-height column>
+    <VLayout class="elevation-1 mx-2">
+      <VBtn icon @click="shiftCurrentWeek(-4)">
+        <VIcon>skip_previous</VIcon>
+      </VBtn>
+      <VBtn class="flip" icon @click="shiftCurrentWeek(-1)">
+        <VIcon>play_arrow</VIcon>
+      </VBtn>
+      <VSpacer />
+      <VBtn icon @click="shiftCurrentWeek(1)">
+        <VIcon>play_arrow</VIcon>
+      </VBtn>
+      <VBtn icon @click="shiftCurrentWeek(4)">
+        <VIcon>skip_next</VIcon>
+      </VBtn>
+    </VLayout>
+    <VLayout justify-center fill-height>
+      <VFlex
+        v-for="week in visibleWeeks"
+        :key="week.weekDate"
+        class="ma-2 xs12 sm6 md4 lg3 xl2"
+      >
+        <ScheduleWeek
+          :week-date="week.weekDate"
+          :current="week.current"
+        />
+      </VFlex>
+    </VLayout>
   </VLayout>
 </template>
 
@@ -75,8 +92,11 @@ export default {
     addWeek (date, weeks) {
       const outputDate = new Date(date)
       const days = weeks * 7
-      outputDate.setDate(outputDate.getDate() + days)
+      outputDate.setUTCDate(outputDate.getUTCDate() + days)
       return this.getDateString(outputDate)
+    },
+    shiftCurrentWeek (weeks) {
+      this.currentWeek = this.addWeek(this.currentWeek, weeks)
     },
     getDateString (date) {
       return date.toISOString().split('T')[0]
@@ -84,3 +104,8 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+.flip
+  transform scaleX(-1)
+</style>
