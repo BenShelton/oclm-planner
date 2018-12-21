1971da7e7975d3709b956e49b9d2d4300dfdccca
BenShelton <bensheltonjones@gmail.com>
2018-12-21 10:18:44 +0000

    add a map of assignment names to types

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const ASSIGNMENT_TYPE_MAP = {
+  chairman: 'CHAIRMAN',
+  openingPrayer: 'PRAYER',
+  talk: 'TALK',
+  gems: 'GEMS',
+  bibleReading: 'BIBLE_READING',
+  serviceTalk1: 'SERVICE_TALK',
+  serviceTalk2: 'SERVICE_TALK',
+  congregationBibleStudy: 'CONGREGATION_BIBLE_STUDY',
+  reader: 'READER',
+  closingPrayer: 'PRAYER'
+}
