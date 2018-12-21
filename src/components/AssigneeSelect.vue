32bbe5fffbf07d572b9fb760ca271c527a772b21
BenShelton <bensheltonjones@gmail.com>
2018-12-21 10:19:08 +0000

    add an assignee selection component

---
+++ src/components/AssigneeSelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <VSelect
+    item-text="name"
+    item-value="name"
+    no-data-text="No Assignees Available"
+    :label="label"
+    :loading="loading"
+    :items="items"
+  />
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'AssigneeSelect',
+
+  props: {
+    value: { type: String, required: true },
+    label: { type: String, required: true }
+  },
+
+  computed: {
+    ...mapGetters({
+      activeMembers: 'congregation/activeMembers',
+      loading: 'congregation/loading'
+    }),
+    items () {
+      return this.activeMembers
+    }
+  }
+}
+</script>
