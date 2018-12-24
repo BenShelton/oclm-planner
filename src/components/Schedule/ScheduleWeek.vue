5d017e8513e3221a33dbf50b023691159f2dcfd5
BenShelton <bensheltonjones@gmail.com>
2018-12-24 13:59:01 +0000

    bind model for assignee dropdowns

--- src/components/Schedule/ScheduleWeek.vue
+++ src/components/Schedule/ScheduleWeek.vue
@@ -122,20 +122,14 @@
                 md6
                 sm4
               >
-                <VTextField
-                  v-model="editAssignment.title"
-                  label="Title"
-                />
+                <VTextField v-model="editAssignment.title" label="Title" />
               </VFlex>
               <VFlex
                 xs12
                 md6
                 sm4
               >
-                <AssigneeSelect
-                  v-model="editAssignment.assignee"
-                  label="Assignee"
-                />
+                <AssigneeSelect v-model="editAssignment.assignee" label="Assignee" />
               </VFlex>
               <VFlex
                 v-if="['INITIAL_CALL', 'RETURN_VISIT', 'BIBLE_STUDY'].includes(editAssignment.type)"
@@ -143,10 +137,7 @@
                 md6
                 sm4
               >
-                <AssigneeSelect
-                  v-model="editAssignment.assistant"
-                  label="Assistant"
-                />
+                <AssigneeSelect v-model="editAssignment.assistant" label="Assistant" />
               </VFlex>
               <VFlex
                 v-if="!(['CHAIRMAN', 'PRAYER', 'READER'].includes(editAssignment.type))"
@@ -154,10 +145,7 @@
                 md6
                 sm4
               >
-                <VTextField
-                  v-model="editAssignment.time"
-                  label="Time"
-                />
+                <VTextField v-model="editAssignment.time" label="Time" />
               </VFlex>
             </VLayout>
           </VContainer>
@@ -297,7 +285,7 @@ export default {
       this.editName = name
       const { displayName, details } = this.assignments[name]
       this.editTitle = `Editing ${displayName} for week ${this.prettyDate}`
-      const assignment = { ...details }
+      const assignment = { assignee: '', ...details }
       if (!assignment.type) assignment.type = ASSIGNMENT_TYPE_MAP[name]
       this.editAssignment = assignment
       this.editDialog = true
