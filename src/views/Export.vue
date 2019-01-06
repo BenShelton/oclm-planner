49b067dc95b682b27e1b66a6a8e96df5f862c6f3
BenShelton <bensheltonjones@gmail.com>
2019-01-06 16:31:53 +0000

    basic layout of pdf complete

--- src/views/Export.vue
+++ src/views/Export.vue
@@ -31,76 +31,123 @@ export default {
 
   methods: {
     generatePDF () {
-      const indentMargin = [8, 0]
+      const weeks = [1, 2, 3, 4, 5]
+
+      const stack = []
       const docDefinition = {
         pageSize: 'A4',
-        pageMargins: [36, 124, 36, 36],
+        pageMargins: [36, 100, 36, 72],
+        pageBreakBefore: ({ id, pageNumbers }) => id === 'PageStartSeparator' && !(pageNumbers.includes(1)),
         header: {
-          margin: 36,
+          margin: [44, 36],
           stack: [
-            {
-              text: 'S-140-EN 12/17',
-              margin: [8, 0, 0, 30]
-            },
+            { text: 'S-140-EN 12/17', margin: [0, 0, 0, 30] },
             {
               columns: [
-                {
-                  text: 'CANTON CONGREGATION',
-                  width: 160,
-                  margin: [8, 4, 0, 0],
-                  bold: true
-                },
-                {
-                  text: 'Our Christian Life & Ministry Schedule',
-                  alignment: 'right',
-                  fontSize: 17,
-                  bold: true
-                }
+                { text: 'CANTON CONGREGATION', width: 160, margin: [0, 4, 0, 0], bold: true },
+                { text: 'Our Christian Life & Ministry Schedule', alignment: 'right', fontSize: 17, bold: true }
               ]
-            },
-            {
-              margin: [0, 8, 0, 0],
-              table: {
-                widths: ['*'],
-                body: [
-                  [{ fillColor: '#333333', text: '' }]
-                ]
-              }
             }
           ]
         },
+        footer (currentPage) {
+          const startingWeek = currentPage * 2 - 1
+          const text = startingWeek === weeks.length ? `WEEK ${startingWeek}` : `WEEKS ${startingWeek} & ${startingWeek + 1}`
+          return { text, margin: [44, 36], alignment: 'right', bold: true }
+        },
         content: [
-          {
-            margin: indentMargin,
-            text: '7 Jan | WEEKLY BIBLE READING: ACTS 21-22',
-            fontSize: 14,
-            bold: true
-          },
-          {
-            margin: indentMargin,
-            text: 'Time Off',
-            bold: true,
-            decoration: 'underline',
-            alignment: 'right'
-          },
-          {
-            margin: indentMargin,
-            table: {
-              widths: [36, '*', 36, 48, 36],
-              body: [
-                [
-                  '7:00',
-                  [{ text: '•', color: COLORS.TREASURES }, 'Song 96']
-                ]
-              ]
-            }
-          }
+          { margin: [8, 0], stack }
         ]
       }
+
+      weeks.forEach((week, index) => {
+        if (index % 2 === 0) stack.push(this.createSeparator(true))
+        stack.push({
+          text: '7 Jan | WEEKLY BIBLE READING: ACTS 21-22',
+          fontSize: 14,
+          bold: true
+        })
+        stack.push({
+          text: 'Time Off',
+          bold: true,
+          decoration: 'underline',
+          alignment: 'right'
+        })
+        stack.push(this.createTable(COLORS.TREASURES, [
+          ['7:00', 'Song 96', 'Prayer:', 'Ben Jones', '7:05'],
+          ['7:05', 'Opening Comments', 'Chairman:', 'Other Brother', '7:08']
+        ]))
+        stack.push(this.createSubheader('TREASURES FROM GOD\'S WORD', COLORS.TREASURES))
+        stack.push(this.createTable(COLORS.MINISTRY, [
+          ['7:08', 'Song 96', 'Prayer:', 'Ben Jones', '7:05'],
+          ['7:05', 'Opening Comments', 'Chairman:', 'Other Brother', '7:08']
+        ]))
+        stack.push(this.createSubheader('APPLY YOURSELF TO THE FIELD MINISTRY', COLORS.MINISTRY))
+        stack.push(this.createTable(COLORS.LIVING, [
+          ['7:08', 'Song 96', 'Prayer:', 'Ben Jones', '7:05'],
+          ['7:05', 'Opening Comments', 'Chairman:', 'Other Brother', '7:08']
+        ]))
+        stack.push(this.createSubheader('LIVING AS CHRISTIANS', COLORS.LIVING))
+        stack.push(this.createTable(COLORS.LIVING, [
+          ['7:08', 'Song 96', 'Prayer:', 'Ben Jones', '7:05'],
+          ['7:05', 'Opening Comments', 'Chairman:', 'Other Brother', '7:08']
+        ]))
+        stack.push(this.createSeparator())
+      })
+
       pdfMake.createPdf(docDefinition).getDataUrl(dataUrl => {
         const previewFrame = this.$refs.pdfPreview
         previewFrame.src = dataUrl
       })
+    },
+
+    createSubheader (text, fillColor) {
+      return {
+        margin: [-8, 0],
+        layout: 'noBorders',
+        table: {
+          widths: [350],
+          body: [
+            [{ margin: [12, 0], color: 'white', text, fillColor }]
+          ]
+        }
+      }
+    },
+
+    createSeparator (pageStart) {
+      return {
+        id: pageStart ? 'PageStartSeparator' : null,
+        margin: [0, 8],
+        stack: [
+          { canvas: [{ type: 'line', x1: -8, y1: 0, x2: 515, y2: 0, lineWidth: 1 }] },
+          { canvas: [{ type: 'line', x1: -8, y1: 3, x2: 515, y2: 3, lineWidth: 1 }] }
+        ]
+      }
+    },
+
+    createTable (markerColor, rows) {
+      const body = []
+      for (const row of rows) {
+        const [timeOn, title, assigneeTitle, assigneeName, timeOff] = row
+        body.push([
+          timeOn,
+          { markerColor, ul: [title] },
+          { text: assigneeTitle, fontSize: 10, color: '#999999', alignment: 'right', margin: [0, 1, 0, 0] },
+          assigneeName,
+          { text: timeOff, alignment: 'right' }
+        ])
+      }
+      return {
+        margin: [0, 8],
+        layout: {
+          defaultBorder: false,
+          fillColor: (rowIndex, node, columnIndex) => rowIndex % 2 === 1 ? '#CCCCCC' : null
+        },
+        table: {
+          widths: [36, '*', 48, 96, 36],
+          body
+        }
+      }
     }
   }
 }
