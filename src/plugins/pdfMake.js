ee4c6f6dbf9baa9ffd5d424f38eddd88365e5503
BenShelton <bensheltonjones@gmail.com>
2019-01-24 20:30:03 +0000

    show the correct month on assignment slips

--- src/plugins/pdfMake.js
+++ src/plugins/pdfMake.js
@@ -231,7 +231,7 @@ export function generateSchedule (weeks, month) {
 function createSlip (assignment, date = '') {
   const { title, type, assignee, assistant, studyPoint } = assignment || {}
   const [y, m, d] = date.split('-')
-  const prettyDate = [d, MONTHS[Number(m) + 1], y].join(' ')
+  const prettyDate = [d, MONTHS[+m - 1], y].join(' ')
   return {
     width: '50%',
     margin: [32, 32, 32, 32],
