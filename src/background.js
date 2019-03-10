d530d6b1ae5774ce8c9f26e1d0c544f62c3af09c
BenShelton <bensheltonjones@gmail.com>
2019-03-10 17:25:11 +0000

    add scripts for serving and building

--- src/background.js
+++ src/background.js
@@ -15,7 +15,10 @@ let win
 protocol.registerStandardSchemes(['app'], { secure: true })
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({
+    width: 1200,
+    height: 800
+  })
 
   if (isDevelopment) {
     // Load the url of the dev server if in development mode
